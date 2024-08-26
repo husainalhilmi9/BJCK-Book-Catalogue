16edec6f328b37f0370f018ad4ddc46844f276f6
husainalhilmi9 <husain.alhilmi@gmail.com>
2024-08-26 16:37:58 +0800

    Update add_book.php

--- book-catalog-app/endpoint/add_book.php
+++ book-catalog-app/endpoint/add_book.php
@@ -10,17 +10,15 @@ $bookTitle = $_POST['book_title'];
 $bookAuthor = $_POST['book_author'];
 $bookPublisher = $_POST['book_publisher'];
 $bookSection = $_POST['book_section'];
-$bookTimeAdded = date("Y-m-d H:i:s");
 
 try {
     // Insert the book into the database without checking for duplicates
-    $stmt = $conn->prepare("INSERT INTO `tbl_book` (`tbl_book_id`, `book_isbn`, `book_title`, `book_author`, `book_publisher`, `book_section`, `time_added`) VALUES (NULL, :bookISBN, :bookTitle, :bookAuthor, :bookPublisher, :bookSection, :bookTimeAdded)");
+    $stmt = $conn->prepare("INSERT INTO `tbl_book` (`tbl_book_id`, `book_isbn`, `book_title`, `book_author`, `book_publisher`, `book_section`) VALUES (NULL, :bookISBN, :bookTitle, :bookAuthor, :bookPublisher, :bookSection)");
     $stmt->bindParam(':bookISBN', $bookISBN);
     $stmt->bindParam(':bookTitle', $bookTitle);
     $stmt->bindParam(':bookAuthor', $bookAuthor);
     $stmt->bindParam(':bookPublisher', $bookPublisher);
     $stmt->bindParam(':bookSection', $bookSection);
-    $stmt->bindParam(':bookTimeAdded', $bookTimeAdded);
     $stmt->execute();
 
     echo "<script>
